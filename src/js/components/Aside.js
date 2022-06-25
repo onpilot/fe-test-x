2fe065888dd88d38733bb363efbf6e6be8e8dd5f
Idan <72357530+onpilot@users.noreply.github.com>
2022-06-26 01:04:19 +0700

    separate Aside from Map component

---
+++ src/js/components/Aside.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import style from '../../css/Aside.module.css';
+import mapMarker from '../../img/icons/description/map-marker.png';
+import globe from '../../img/icons/description/globe.png';
+import { description } from '../../data/description';
+
+const Aside = ({ selectedCity }) => {
+  return (
+    <aside className={style.aside}>
+      <img
+        src={description[selectedCity].img}
+        alt={selectedCity}
+        className={style.aside__img}
+      />
+      <h1 className={style.aside__title}>{selectedCity}</h1>
+      <div className={style.aside__text}>
+        {description[selectedCity].text.map((e, i) => (
+          <p key={`${selectedCity}_text_${i}`}>{e}</p>
+        ))}
+        <div className={style.aside__text_extras}>
+          <img src={mapMarker} alt='map-marker' />
+          <span>{description[selectedCity].address}</span>
+        </div>
+        <div className={style.aside__text_extras}>
+          <img src={globe} alt='globe' />
+          <a href={description[selectedCity].web}>
+            {description[selectedCity].web}
+          </a>
+        </div>
+      </div>
+    </aside>
+  );
+};
+
+export default Aside;
--- src/js/components/Map.js
+++ src/js/components/Map.js
@@ -1,38 +1,12 @@
 import React from 'react';
 import style from '../../css/Map.module.css';
-import mapMarker from '../../img/icons/description/map-marker.png';
-import globe from '../../img/icons/description/globe.png';
-import { description } from '../../data/description';
+import Aside from './Aside';
 
 const Map = ({ selectedCity }) => {
   return (
     <div className={style.map}>
       <div>map</div>
-      {selectedCity && (
-        <aside className={style.aside}>
-          <img
-            src={description[selectedCity].img}
-            alt={selectedCity}
-            className={style.aside__img}
-          />
-          <h1 className={style.aside__title}>{selectedCity}</h1>
-          <div className={style.aside__text}>
-            {description[selectedCity].text.map((e, i) => (
-              <p key={`${selectedCity}_text_${i}`}>{e}</p>
-            ))}
-            <div className={style.aside__text_extras}>
-              <img src={mapMarker} alt='map-marker' />
-              <span>{description[selectedCity].address}</span>
-            </div>
-            <div className={style.aside__text_extras}>
-              <img src={globe} alt='globe' />
-              <a href={description[selectedCity].web}>
-                {description[selectedCity].web}
-              </a>
-            </div>
-          </div>
-        </aside>
-      )}
+      {selectedCity && <Aside selectedCity={selectedCity} />}
     </div>
   );
 };
