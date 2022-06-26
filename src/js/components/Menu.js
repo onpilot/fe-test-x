0860cb8aafc685c1bf5c11e50078856ea117cb54
Idan <72357530+onpilot@users.noreply.github.com>
2022-06-27 05:32:27 +0700

    feat: enable select/deselect menu

--- src/js/components/Menu.js
+++ src/js/components/Menu.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import style from '../../css/Menu.module.css';
 import { place } from '../../data/place';
 
-const Menu = ({ setSelectedCity }) => {
-  const handleClick = (city, target) => {
-    setSelectedCity(city);
-
-    const active = document.querySelector(
+const Menu = ({ selectedCity, setSelectedCity }) => {
+  const deselectMenu = () => {
+    const prevSelected = document.querySelector(
       `.${style.menu__bottom_city__selected}`
     );
-    active && active.classList.toggle(`${style.menu__bottom_city__selected}`);
+    prevSelected &&
+      prevSelected.classList.toggle(`${style.menu__bottom_city__selected}`);
+  };
 
+  const handleClick = (city, target) => {
+    setSelectedCity(city);
+    deselectMenu();
     target.classList.toggle(`${style.menu__bottom_city__selected}`);
   };
+
+  useEffect(() => {
+    if (selectedCity) {
+      const city = selectedCity.split(' ').join('').toLowerCase();
+      const id = `menu_${city}`;
+      const toBeSelected = document.getElementById(id);
+
+      deselectMenu();
+      toBeSelected.classList.toggle(`${style.menu__bottom_city__selected}`);
+    }
+  }, [selectedCity]);
 
   return (
     <div className={style.menu}>
@@ -20,21 +34,25 @@ const Menu = ({ setSelectedCity }) => {
         <div className={style.menu__top_filter}>Filter by Favorite</div>
       </div>
 
-      {place.map(({ city, list }) => (
-        <div className={style.menu__bottom} key={city}>
-          <div
-            className={style.menu__bottom_city}
-            onClick={(e) => handleClick(city, e.currentTarget)}
-          >
-            {city}
+      {place.map(({ city, list }) => {
+        const id = `menu_${city.split(' ').join('').toLowerCase()}`;
+        return (
+          <div className={style.menu__bottom} key={city}>
+            <div
+              id={id}
+              className={style.menu__bottom_city}
+              onClick={(e) => handleClick(city, e.currentTarget)}
+            >
+              {city}
+            </div>
+            <ul>
+              {list.map((e) => (
+                <li key={e}>{e}</li>
+              ))}
+            </ul>
           </div>
-          <ul>
-            {list.map((e) => (
-              <li key={e}>{e}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
